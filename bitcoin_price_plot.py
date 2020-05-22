94f9c1b03f52c43929e9bb06ec7dc8295e493472
anupkhadka <khadkaanup2020@gmail.com>
2020-05-21 21:10:16 -0400

    Read closing price of bitcoin for each day of 01/01/2020 to today's date from a web API provided by coindesk, then plot the data using matplotlib.

---
+++ bitcoin_price_plot.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python 
+
+from datetime import date
+import json
+import matplotlib.pyplot as plt
+import requests
+
+date_list = []
+price_list = []
+
+today = date.today()
+today = today.strftime("%Y-%m-%d")
+
+# pull in json data of bitcoin price from Jan 1, 2017 to Dec 31, 2017 from api.coindesk.com
+url = f'https://api.coindesk.com/v1/bpi/historical/close.json?start=2020-01-01&end={today}' 
+response = requests.get(url)
+data = response.json()
+date_price_dict = data['bpi']
+
+for date,price in date_price_dict.items():
+  date_list.append(date)
+  price_list.append(price)
+
+plt.plot(date_list, price_list)
+plt.title(f'Bitcoin price change from 2020-01-01 to {today}')
+plt.show()
